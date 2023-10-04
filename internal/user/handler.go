49c2f31370aee73e285c46dd864edfa54828561e
Tati <dev.tm.mironova@mail.ru>
2023-10-04 18:21:03 +0300

    add logging, config

---
+++ internal/user/handler.go
@@ -0,0 +1,63 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/taaanechka/rest-api-go/internal/handlers"
+	"github.com/taaanechka/rest-api-go/pkg/logging"
+)
+
+const (
+	usersURL = "/users"
+	userURL  = "/users/:uuid"
+)
+
+type handler struct {
+	logger *logging.Logger
+}
+
+func NewHandler(logger *logging.Logger) handlers.Handler {
+	return &handler{
+		logger: logger,
+	}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.POST(usersURL, h.CreateUser)
+	router.GET(userURL, h.GetUserByUUID)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is list of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("this is create user"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is user by uuid"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is update user"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is delete user"))
+}
